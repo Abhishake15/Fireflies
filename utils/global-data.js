6aa49b180e87781063d2885c29e14514f8135738
Abhishake15 <104545474+Abhishake15@users.noreply.github.com>
2023-12-07 18:06:05 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'By Abhishek Agarwal';
+    : 'World Of Insects';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'World Of Insects';
+    : 'By Abhishek Agarwal';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
